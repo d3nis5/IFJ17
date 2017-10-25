5994dae0c43287e5d8276b1e0857d0fbff9f1b77
Denis Leitner <5197denis@gmail.com>
2017-10-25 20:52:39 +0200

    prvy den

--- parser.c
+++ parser.c
@@ -1,1 +1,273 @@
 #include "parser.h"
+
+int err_code = 0;
+
+bool r_program()
+{
+
+	if ( (token == Scope) || (token == Declare) || (token == Function))
+	{
+		/* Simulacia pravidla '1' */
+		if ( r_fc_dec() == false )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		if ( token != KWD_scope )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		if ( r_stat_list() == false )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+		
+		if ( token != KWD_end )
+			return false;
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		token = get_token();
+		
+		if ( token != KWD_scope )
+			return false;
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+		
+		token = get_token();
+
+		if ( token != EOL )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		if ( token != TKN_EOF )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+				
+	}
+	else
+		/* syntakticka chyba */ 
+		return false;
+
+	return true;
+}
+
+
+bool r_fc_dec()
+{
+	if ( token == KWD_declare )
+	{
+		/* Simulacia pravidla '2' */
+		
+		if ( r_declaration() == false )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		if ( token != EOL )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+	
+		token = get_token();
+
+		if ( r_fc_dec() == false )	
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+	}
+	else if ( token == KWD_function )
+	{
+		/* Simulacia pravidla '3' */
+
+		if ( r_definition() == false )		
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+		
+		if ( token != EOL )	
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		token = get_token();
+
+		if ( r_fc_dec() == false )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+	}
+	else if ( token == KWD_scope )
+	{
+		/* Simulacia pravidla '4' 
+		 * epsilon pravidlo
+		 */
+		
+		return true;
+	}
+	else
+	{
+		/* Syntakticka chyba */
+
+		//err_code = SYNTAX_ERR;
+		return false;
+	}
+	return true;
+}
+
+
+bool r_declaration()
+{
+	if ( token == KWD_declare )
+	{
+		/* Simulacia pravidla '5' */
+	
+		token = get_token();
+
+		if ( token != KWD_function )	
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		token = get_token();
+
+		if ( token != ID )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		token = get_token();
+
+		if ( token != left_par )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		token = get_token();
+		
+		if ( r_item_list() == false )	
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		if ( token != right_par )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+		
+		token = get_token();
+
+		if ( token != KWD_as )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		token = get_token();
+
+		if ( r_type() == false )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		if ( token != EOL )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		token = get_token();
+	
+		if ( r_stat_list() == false )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		if ( token != KWD_end )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+
+		token = get_token();
+
+		if ( token != KWD_function )
+		{
+			//err_code = SYNTAX_ERR;
+			return false;
+		}
+	}
+	else
+	{
+		/* Syntakticka chyba */
+		//err_code = SYNTAX_ERR;
+		return false;
+	}
+
+}
+
+
+bool r_definition();
+
+
+bool r_var_declaration();
+
+
+bool r_var_definition();
+
+
+bool r_assign();
+
+
+bool r_expr_list();
+
+
+bool r_item_list();
+
+
+bool r_item2_list();
+
+
+bool r_par_list();
+
+
+bool r_par2_list();
+
+
+bool r_rhs();
+
+
+bool r_stat();
+
+
+bool r_stat_list();
+
+
+bool r_type();
--- parser.h
+++ parser.h
@@ -1,4 +1,86 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Pravidlo pre <Program> */
+bool r_program();
+
+/* Pravidlo pre <FceDec> 
+ * Deklaracia a definicia funkcii
+ */
+bool r_fc_dec();
+
+/* Pravidlo pre <Declaration>
+ * Deklaraacia funkcii
+ */
+bool r_declaration();
+
+/* Pravidlo pre <Definition> 
+ * Definicia funkcii
+ */
+bool r_definition();
+
+/* Pravidlo pre <VarDec> 
+ * Deklaracia a definicia premennych
+ */
+bool r_var_declaration();
+
+/* Pravidlo pre <VarDef> 
+ * Definicia premennych
+ */
+bool r_var_definition();
+
+/* Pravidlo pre <Assign> 
+ * Priradenie hodnoty do premennej alebo zavolanie funkcie na pravej strane vyrazu
+ */
+bool r_assign();
+
+/* Pravidlo pre <ExprList> 
+ * Zoznam vyrazov pre funkciu print
+ */
+bool r_expr_list();
+
+/* Pravidlo pre <ItemList> 
+ * Zoznam parametrov pri deklarovani/ definicii funcii
+ */
+bool r_item_list();
+
+/* Pravidlo pre <Item2List> 
+ * Pomocne pravidlo pre pravidlo <ItemList>
+ */
+bool r_item2_list();
+
+/* Pravidlo pre <ParList> 
+ * Zoznam parametrov funkcie pri jej volani
+ */
+bool r_par_list();
+
+/* Pravidlo pre <Par2List>
+ * Pomocne pravidlo pre pravidlo <ParList>
+ */
+bool r_par2_list();
+
+/* Pravidlo pre <RS> 
+ * Prava strana priradenia pri pravidle <Assign>
+ */
+bool r_rhs();
+
+/* Pravidlo pre <Stat> 
+ * Statement - prikaz
+ */
+bool r_stat();
+
+/* Pravidlo pre <StatList> 
+ * Statement list - Zoznam prikazov 
+ */
+bool r_stat_list();
+
+/* Pravidlo pre <Type> 
+ * Datove typy
+ */
+bool r_type();
+
 #endif /* PARSER_H */
