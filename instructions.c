8ac1b0ffb8b6e52e7951527a3a92dc4132fa486d
Denis Leitner <5197denis@gmail.com>
2017-11-20 10:07:52 +0100

    paska instrukcii

---
+++ instructions.c
@@ -0,0 +1,71 @@
+#include "instructions.h"
+
+void init_list(instruction_lst *list)
+{
+	list->first = NULL;
+	list->last = NULL;
+}
+
+
+bool add_instruction(instruction_lst *list, char *inst)
+{
+	char *new_instruction = malloc(strlen(inst) * sizeof(char) + 1);
+	if ( new_instruction == NULL )
+	{
+		err_code = COMPILER_ERR;
+		return false;
+	}
+	strcpy(new_instruction, inst);
+	instruction_t *new = malloc(sizeof(instruction_t));
+	if ( new == NULL )
+	{
+		err_code = COMPILER_ERR;
+		return false;
+	}
+
+	new->ins = new_instruction;
+	new->next = NULL;
+
+	if ( list->last != NULL )
+		list->last->next = new;
+	list->last = new;
+	
+	
+	if ( list->first == NULL )	/* zoznam bol prazdny */
+	{
+		list->first = new;
+	}
+
+	return true;
+}
+
+void print_list(instruction_lst list)
+{
+	instruction_t *tmp = list.first;
+	
+	while ( tmp != NULL )
+	{
+		printf("%s\n", tmp->ins);
+		tmp = tmp->next;	
+	}
+}
+
+void delete_list(instruction_lst *list)
+{
+	if ( list->first == NULL )	/* prazdny zoznam */
+		return;
+	instruction_t *act = list->first;	/* aktualny prvok */
+	instruction_t *next = act->next;	/* nasledujuci prvok */
+
+	while ( next != NULL )
+	{
+		free(act->ins);		
+		free(act);
+		act = next;
+		next = act->next;
+	}
+	free(act->ins);		
+	free(act);
+	free(list);
+	list = NULL;
+}
